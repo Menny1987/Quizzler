2650c86360f4b15f1adbeec48ddf76cc25051914
Menny1987 <Mennyyyy@gmail.com>
2018-01-16 23:31:54 +0200

    creating the app!

---
+++ Quizzler/Questsion.swift
@@ -0,0 +1,23 @@
+//
+//  Questsion.swift
+//  Quizzler
+//
+//  Created by Menny Atia on 13/01/2018.
+//  Copyright © 2018 Menny Atia. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+    
+   
+    
+}
